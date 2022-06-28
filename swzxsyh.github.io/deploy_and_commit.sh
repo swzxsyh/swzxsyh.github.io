151c8ac9f3fac11a0f1763fe5c95b3519ac08186
swzxsyh <swzxsyh@users.noreply.github.com>
2022-06-29 02:43:11 +0800

    backup

--- swzxsyh.github.io/deploy_and_commit.sh
+++ swzxsyh.github.io/deploy_and_commit.sh
@@ -1,9 +1,9 @@
 hexo d -g &&
 hexo clean &&
 git checkout hexo_source &&
-git merge master &&
 git add . &&
-git add git add ../oh-my-zsh/custom/plugins && 
-git commit 'backup' &&
+git add ../oh-my-zsh && 
+git commit -m 'backup' &&
+git merge master &&
 git push git@github.com:swzxsyh/swzxsyh.github.io.git hexo_source &&
 git checkout master
